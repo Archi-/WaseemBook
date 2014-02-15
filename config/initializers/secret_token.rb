ccf174f408daf7ae6b7cf31d3558dfa9e0976f5f
Archio <waseemjr@gmail.com>
2014-02-15 05:20:33 +0100

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WaseemBook::Application.config.secret_key_base = 'e7784f1fd62ab07a9fef4f826bb4f10a10c50b519b18631961e05b36d37cdb8a40b2d91eac6601037dcee91bfc93d436c8457c05f495548ea8766fcada9aba16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WaseemBook::Application.config.session_store :cookie_store, key: '_WaseemBook_session'
